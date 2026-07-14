ea208fed64ac44e52394c34f8389656d0d9ddd06
VBCoCo <rcontro@gmx.de>
2026-07-14 07:53:17 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Einmalig mit den Werten aus Ihrem Supabase-Projekt ausfuellen.
 // Ohne diese Werte funktioniert die App weiterhin lokal auf einem Geraet.
 window.TURNIER_CLOUD_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://rbmfxozcpfxxdhrtypxj.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Wvt7b4s2ysIAbtSzh_-6LA_sgx917hQ'
 };
